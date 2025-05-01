f7cbf2a583a94856bae12e319404a08ada128e5f
Kyle Parato <kyleparato@gmail.com>
2025-05-01 04:15:38 -0700

    Test secret

--- amplifyConfig.ts
+++ amplifyConfig.ts
@@ -1,6 +1,16 @@
 // amplifyConfig.ts
 import { Amplify } from 'aws-amplify';
+import {secret } from '@aws-amplify/backend';
 // path to the file the sandbox just generated:
 import outputs from './amplify/amplify_outputs.json';
 
-Amplify.configure(outputs);
+Amplify.configure({
+  API: {
+    GraphQL: {
+      endpoint: 'https://6tgwnqdj6je23laqw2hl5zxb2u.appsync-api.us-east-1.amazonaws.com/graphql',
+      region: 'us-east-1',
+      defaultAuthMode: 'apiKey',
+      apiKey: secret("apiKey")
+    }
+  }
+}, outputs);
